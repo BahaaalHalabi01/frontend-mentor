cff1e87750cdc3a15346e5896ec197bff34abad0
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-01-19 22:59:26 +0300

    basic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+</script>
+
+<main class="min-h-screen flex pt-24 px-64">
+	<section class="container mx-auto flex items-start justify-center">
+		<div class="bg-white p-8 rounded-2xl max-w-4xl">
+			<picture>
+				<img alt="omlette" src="/recipe-page/image-omelette.jpeg" class="rounded-xl" />
+			</picture>
+			<h1 class="text-5xl py-8">Simple Omelette Recipe</h1>
+		</div>
+	</section>
+</main>
+
+<style>
+	* {
+		--white: hsl(0, 0%, 100%);
+		--rose-white: hsl(330, 100%, 98%);
+		--eggshell: hsl(30, 54%, 90%);
+		--light-grey: hsl(30, 18%, 87%);
+		--wenge-brown: hsl(30, 10%, 34%);
+		--dark-charcoal: hsl(24, 5%, 18%);
+		--nutmeg: hsl(14, 45%, 36%);
+		--dark-raspberry: hsl(332, 51%, 32%);
+	}
+
+	h1 {
+		font-family: 'Young Serif',serif;
+	}
+
+	main {
+		background-color: var(--eggshell);
+		@apply text-lg;
+	}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    container: {
+      sm: '576px',
+      md: '960px',
+      lg: '1440px',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
