f86438c31d9d3e2d5880be4bbd06ac5363f858de
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-01-24 03:54:47 +0300

    desktop

---
+++ src/routes/newsletter/+page.server.ts
@@ -0,0 +1,20 @@
+import type { Actions } from './$types';
+import * as z from 'zod';
+
+export const actions = {
+	default: async ({ request }) => {
+		const d = await request.formData();
+		const email = d.get('email');
+
+		const result = z.string().email('Valid email required').safeParse(email);
+
+		if (!result.success) return { success: false, error: result.error.format()._errors[0] };
+
+
+		return {
+      success: true,
+      email
+    };
+
+	}
+} satisfies Actions;
--- src/routes/newsletter/+page.svelte
+++ src/routes/newsletter/+page.svelte
@@ -1,59 +1,62 @@
 <script lang="ts">
-	import type { FormEventHandler } from 'svelte/elements';
-	import * as z from 'zod';
+	import { enhance } from '$app/forms';
+	let { form } = $props();
 
-	let error = $state('');
-
-	const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-		const d = new FormData(e.currentTarget);
-
-		const email = d.get('email');
-
-		const result = z.string().email('Valid email required').safeParse(email);
-
-		if (!result.success) {
-			error = result.error.format()._errors[0];
-			return;
-		}
-	};
+  function reset(){
+    window.location.reload()
+  }
 </script>
 
 <main>
-	<div class="bg-white lg:p-6 md:rounded-3xl flex mx-auto gap-y-8 justify-center max-w-fit">
-		<section class="col-span-1 flex flex-col items-start justify-center pl-10 pr-16 gap-y-4">
-			<h1>Stay updated!</h1>
-			<h2 class="w-[376px]">Join 60,000+ product managers receiving monthly updates on:</h2>
-			<ol>
-				<li><span>Product discovery and building what matters</span></li>
-				<li><span>Measuring to ensure updates are a success</span></li>
-				<li><span>And much more!</span></li>
-			</ol>
-			<form class="min-w-full gap-y-6 flex flex-col" on:submit|preventDefault={handleSubmit}>
-				<label class="min-w-full">
-					<div class="flex justify-between text-xs pb-2 font-bold">
-						<span class="block">Email address</span>
-						{#if error.length > 0}
-							<span class="error">{error}</span>
-						{/if}
-					</div>
-					<input
-						aria-errormessage={error ?? undefined}
-						aria-invalid={!!error}
-						autocomplete="email"
-						name="email"
-						type="text"
-						class:error
-						placeholder={'email@company.com'}
-					/>
-				</label>
-				<button class="h-14 flex items-center w-full rounded-md justify-center">
-					Subscribe to monthly newsletter
-				</button>
-			</form>
-		</section>
-		<picture class="max-h-fit">
-			<img alt="newsletter" src="/newsletter/illustration-sign-up-desktop.svg" />
-		</picture>
+	<div class="bg-white lg:p-6 md:rounded-[32px] flex mx-auto gap-y-8 justify-center max-w-fit">
+		{#if !form?.success}
+			<section class="col-span-1 flex flex-col items-start justify-center pl-10 pr-16 gap-y-4">
+				<h1>Stay updated!</h1>
+				<h2 class="w-[376px]">Join 60,000+ product managers receiving monthly updates on:</h2>
+				<ol>
+					<li><span>Product discovery and building what matters</span></li>
+					<li><span>Measuring to ensure updates are a success</span></li>
+					<li><span>And much more!</span></li>
+				</ol>
+				<form class="min-w-full gap-y-6 flex flex-col" use:enhance target="_self" method="POST">
+					<label class="min-w-full">
+						<div class="flex justify-between text-xs pb-2 font-bold">
+							<span class="block">Email address</span>
+							{#if form?.error && form?.error?.length > 0}
+								<span class="error">{form.error}</span>
+							{/if}
+						</div>
+						<input
+							aria-errormessage={!form?.success ? form?.error : undefined}
+							aria-invalid={form?.success}
+							autocomplete="email"
+							name="email"
+							type="text"
+							class:error={form?.success === false}
+							placeholder={'email@company.com'}
+						/>
+					</label>
+					<button class="h-14 flex items-center w-full rounded-md justify-center">
+						Subscribe to monthly newsletter
+					</button>
+				</form>
+			</section>
+			<picture class="max-h-fit">
+				<img alt="newsletter" src="/newsletter/illustration-sign-up-desktop.svg" />
+			</picture>
+		{:else}
+			<section
+				class="col-span-1 flex flex-col items-start justify-center px-8 pb-8 pt-4 max-w-[400px] gap-y-4"
+			>
+				<img src="/newsletter/icon-success.svg" alt="success" />
+				<h1 class='pt-4'>Thanks for subscribing!</h1>
+				<p class='pt-2 pb-5'>
+					A confirmation email has been sent to <span class='font-bold'>{form?.email}</span>. Please open it and click the
+					button inside to confirm your subscription.
+				</p>
+				<button class="btn-success" onclick={reset}> Dismiss message </button>
+			</section>
+		{/if}
 	</div>
 </main>
 
@@ -73,8 +76,13 @@
 		color: var(--dark-slate-grey);
 		@apply min-h-screen flex flex-col items-center justify-center p-20;
 	}
+	p {
+		line-height: 1.4;
+		font-size: 14px;
+	}
 	h1 {
 		font-size: 50px;
+		line-height: 1;
 		@apply font-bold;
 	}
 	button {
@@ -97,6 +105,12 @@
 	button {
 		background-color: var(--dark-slate-grey);
 	}
+
+	.btn-success {
+		box-shadow: 0 16px 40px -8px var(--tomato);
+		@apply h-12 flex items-center w-full rounded-md justify-center bg-[var(--tomato)] outline-none text-sm;
+	}
+
 	button:focus-visible {
 		box-shadow: 0 16px 40px -8px var(--tomato);
 		@apply bg-[var(--tomato)] outline-none;
