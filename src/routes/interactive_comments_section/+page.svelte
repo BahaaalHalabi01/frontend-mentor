2bc20ccf6bec6c2e487b4f3393ec0dbf0852a77b
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-01-31 23:18:35 +0300

    basic card

---
+++ src/routes/interactive_comments_section/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+  import CommentCard from './CommentCard.svelte'
+</script>
+
+<svelte:head>
+	<title>Interactive Comments Section</title>
+</svelte:head>
+
+<main> 
+<div class='max-w-3xl w-full'>
+  <CommentCard/>
+</div>
+
+</main>
+
+<style>
+	* {
+		--dark-blue: hsl(212, 24%, 26%);
+		--grayish-blue: hsl(211, 10%, 45%);
+		--light-gray: hsl(223, 19%, 93%);
+		--very-light-gray: hsl(228, 33%, 97%);
+		--white: hsl(0, 0%, 100%);
+
+		--moderate-blue: hsl(238, 40%, 52%);
+		--soft-red: hsl(358, 79%, 66%);
+		--light-grayish-blue: hsl(239, 57%, 85%);
+		--pale-red: hsl(357, 100%, 86%);
+	}
+
+	main {
+		font-family: 'Rubik', sans-serif;
+    background-color:var(--light-gray);
+		@apply min-h-full pt-12 md:pt-20 flex flex-col items-center ;
+	}
+
+	@font-face {
+		font-family: Rubik;
+		font-weight: 400 500 700;
+		font-style: normal;
+		src: local('Rubik'),
+			urlurl('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
+	}
+</style>
---
+++ src/routes/interactive_comments_section/CommentCard.svelte
@@ -0,0 +1,10 @@
+<div class="flex min-w-full gap-x-6 rounded-lg bg-white p-6 shadow">
+	<div class="flex flex-col bg-[var(--very-light-gray)] rounded-xl items-center gap-y-2 shadow">
+		<button class='h-9 w-10'>+</button>
+		12
+		<button class='h-9 w-10'>-</button>
+	</div>
+	<div class="flex flex-col grow">
+		<div class='flex items-center gap-x-4'><img src='/interactive_comments/avatars/image-amyrobson.webp' alt='amyrobson' width={36} /><span>amyrobson</span><span> 1 month ago</span> <button class='ml-auto'>reply</button></div>
+	</div>
+</div>
