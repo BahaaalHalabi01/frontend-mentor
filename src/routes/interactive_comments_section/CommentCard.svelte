95e9b168306fe5b21d7544610f5a15b612bd5c80
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-02-05 04:53:08 +0300

    small design

--- src/routes/interactive_comments_section/CommentCard.svelte
+++ src/routes/interactive_comments_section/CommentCard.svelte
@@ -7,25 +7,49 @@
 		replies: TReply[];
 	}>();
 
-	current_user.subscribe((v) => console.log(v));
+	current_user.subscribe(function () {});
 </script>
 
-{#snippet entry(params:TReply | TComment)}
-	<div class="flex min-w-full gap-x-6 rounded-lg bg-white p-6 shadow flex-col md:flex-row">
-		<div
-			class="flex h-fit flex-col items-center gap-y-2 rounded-lg bg-[var(--very-light-gray)] shadow order-1 md:order-none"
-		>
-			<button class="increment">
-				<img src="/interactive_comments/icon-plus.svg" alt="+" />
+{#snippet actions(username:string)}
+	{#if $current_user?.username === username}
+		<div class="ml-auto flex gap-x-6">
+			<button class="btn text-[var(--soft-red)] hover:bg-red-300">
+				<img src="/interactive_comments/icon-delete.svg" alt="delete" class="inline-flex pr-1.5" />
+				Delete
 			</button>
-			<span class="font-semibold text-[var(--moderate-blue)]">
-				{params.score}
-			</span>
-			<button class="increment">
-				<img src="/interactive_comments/icon-minus.svg" alt="-" />
+			<button class=" btn text-[var(--moderate-blue)] hover:bg-red-300">
+				<img src="/interactive_comments/icon-edit.svg" alt="edit" class="inline-flex pr-1.5" />
+				Edit
 			</button>
 		</div>
+	{:else}
+		<button class=" btn text-[var(--moderate-blue)] hover:bg-red-300">
+			<img src="/interactive_comments/icon-reply.svg" alt="goback" class="" />
+			Reply
+		</button>
+	{/if}
+{/snippet}
 
+{#snippet entry(params:TReply | TComment)}
+	<div class="flex min-w-full flex-col gap-x-6 rounded-lg bg-white p-4 shadow md:flex-row md:p-6">
+		<div class=" order-1 flex items-center justify-between pt-4 md:order-none md:pt-0">
+			<div
+				class="inline-flex size-fit flex-row items-center gap-y-2 rounded-lg bg-[var(--very-light-gray)] shadow md:flex-col"
+			>
+				<button class="increment">
+					<img src="/interactive_comments/icon-plus.svg" alt="+" />
+				</button>
+				<span class="font-semibold text-[var(--moderate-blue)] px-1.5 md:px-0">
+					{params.score}
+				</span>
+				<button class="increment">
+					<img src="/interactive_comments/icon-minus.svg" alt="-" />
+				</button>
+			</div>
+			<div class="md:hidden">
+				{@render actions(params.user.username)}
+			</div>
+		</div>
 
 		<div class="flex grow flex-col gap-y-3">
 			<div class="flex items-center gap-x-4">
@@ -35,43 +59,21 @@
 					width={30}
 				/>
 				<span
-					class="flex items-center gap-x-2 font-bold text-[var(--grayish-blue)]"
+					class="flex items-end gap-x-2 font-bold text-[var(--grayish-blue)]"
 					aria-label="username"
 					>{params.user.username}
 					{#if $current_user?.username === params.user.username}
 						<span
-							class="flex h-fit items-center justify-center rounded-sm bg-[var(--moderate-blue)] px-1 py-0.5 text-xs font-semibold leading-none text-white"
+							class="flex h-fit items-center justify-center rounded-sm bg-[var(--moderate-blue)] p-1 text-xs font-semibold leading-none text-white"
 							>you</span
 						>
 					{/if}
 				</span>
 
 				<span class="text-[var(--grayish-blue)]" aria-label="created at">{params.createdAt}</span>
-				{#if $current_user?.username === params.user.username}
-					<div class="ml-auto flex gap-x-6">
-						<button class="ml-auto rounded-md font-bold text-[var(--soft-red)] hover:bg-red-300">
-							<img
-								src="/interactive_comments/icon-delete.svg"
-								alt="delete"
-								class="inline-flex pr-1"
-							/>
-							Delete
-						</button>
-						<button
-							class="ml-auto rounded-md font-bold text-[var(--moderate-blue)] hover:bg-red-300"
-						>
-							<img src="/interactive_comments/icon-edit.svg" alt="edit" class="inline-flex pr-1" />
-							Edit
-						</button>
-					</div>
-				{:else}
-					<button class="ml-auto rounded-md font-bold text-[var(--moderate-blue)] hover:bg-red-300">
-						<img src="/interactive_comments/icon-reply.svg" alt="goback" class="inline-flex pr-1" />
-						Reply
-					</button>
-				{/if}
+				<div class="hidden md:block">{@render actions(params.user.username)}</div>
 			</div>
-			<p aria-label="comment" class="pr-3 text-base font-normal text-[var(--grayish-blue)]">
+			<p aria-label="comment" class="pr-3 text-base font-normal text-[var(--grayish-blue)] leading-snug md:leading-loose">
 				{#if (params as TReply).replyingTo}
 					<span class="font-bold text-[var(--moderate-blue)]">
 						@{(params as TReply).replyingTo}</span
@@ -83,11 +85,11 @@
 	</div>
 {/snippet}
 
-<section>
+<div>
 	{@render entry(comment)}
 	{#if replies.length > 0}
-		<div class="flex gap-x-8 pt-4">
-			<div class="border-r-2 border-[var(--grayish-blue)] pl-8"></div>
+		<div class="flex gap-x-4 pt-4 md:gap-x-8">
+			<div class="border-r-2 border-gray-300 md:pl-8"></div>
 			<div class="flex flex-col gap-y-4">
 				{#each replies as reply}
 					{@render entry(reply)}
@@ -95,10 +97,18 @@
 			</div>
 		</div>
 	{/if}
-</section>
+</div>
 
 <style>
+	.btn {
+		@apply ml-auto flex items-center rounded-md font-semibold;
+	}
+
+	.btn > img {
+		@apply inline-flex pr-1.5;
+	}
+
 	.increment {
-		@apply flex h-7 w-8 items-center justify-center rounded-lg hover:bg-red-300;
+		@apply flex size-8 items-center justify-center rounded-lg hover:bg-red-300 md:h-7 md:w-8;
 	}
 </style>
