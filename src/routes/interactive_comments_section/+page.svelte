e26b561c64fb360272c12ec9dad0b75c43092e96
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-02-29 23:03:41 +0300

    design

--- src/routes/interactive_comments_section/+page.svelte
+++ src/routes/interactive_comments_section/+page.svelte
@@ -3,7 +3,7 @@
 	import type { PageData } from './$types';
 	import CommentCard from './CommentCard.svelte';
 	import { createUser } from './user.svelte.ts';
-	import { item_id } from './replying.svelte';
+	// import { item_id } from './replying.svelte';
 
 	const { data } = $props<{ data: PageData }>();
 
@@ -13,8 +13,6 @@
 		replying?: boolean;
 		id?: string;
 	};
-
-
 </script>
 
 <svelte:head>
@@ -23,56 +21,59 @@
 
 {#snippet textInput(input:TTextInput)}
 	<div
-		class="flex min-w-full flex-wrap gap-y-4 rounded-lg bg-white p-4 shadow md:flex-row md:items-start md:gap-x-4 md:p-6"
+		class="flex min-w-full flex-wrap gap-y-4 rounded-lg bg-white px-2 py-4 shadow md:flex-row md:items-start md:gap-x-4 md:p-6 md:gap-x-4"
 	>
-		<img
-			src={`/interactive_comments/${user.image}`}
-			alt={user.username}
-			class="inline-flex h-fit"
-			width={40}
-		/>
-		{#if input.id === item_id}
-			<input class="hidden" name="id" value={input.id} />
-			<input class="hidden" name="replyingTo" value={input.replying} />
-		{/if}
+		<div class="inline-flex grow gap-x-1 md:gap-x-4">
+			<img
+				src={`/interactive_comments/${user.image}`}
+				alt={user.username}
+				class="inline-flex h-fit"
+				width={40}
+			/>
 
-		<textarea
-			class=" inline-flex grow resize-none rounded-lg border border-gray-300 px-6 py-2 placeholder:text-[var(--grayish-blue)]"
-			placeholder={input ? 'Add a reply...' : 'Add a comment...'}
-			aria-label={input ? 'New Reply' : 'New Comment'}
-			rows="3"
-			name="comment"
-			maxlength="1000"
-			spellcheck="true"
-			title={input ? 'add a reply' : 'add a comment'}
-		/>
-
+			<textarea
+				class=" inline-flex w-full grow resize-none rounded-lg border border-gray-300 px-4 py-2 placeholder:text-[var(--grayish-blue)] md:w-fit"
+				placeholder={input ? 'Add a reply...' : 'Add a comment...'}
+				aria-label={input ? 'New Reply' : 'New Comment'}
+				rows="3"
+				name="comment"
+				maxlength="1000"
+				spellcheck="true"
+				title={input ? 'add a reply' : 'add a comment'}
+			/>
+		</div>
+		<input class="hidden" name="id" value={input.id} />
+		<input class="hidden" name="replyingTo" value={input.replying} />
 		<button
-			class="ml-auto inline-flex max-w-fit rounded-md bg-[var(--moderate-blue)] px-6 py-2.5 font-medium uppercase text-white hover:opacity-30"
+			class="ml-auto inline-flex max-w-fit basis-auto flex-wrap rounded-md bg-[var(--moderate-blue)] px-6 py-2.5 font-medium uppercase text-white hover:opacity-30"
 		>
-			{input ? 'Reply' : 'Send'}
+			{input.replying ? 'Reply' : 'Send'}
 		</button>
 	</div>
 {/snippet}
 
 <main>
-	<form class="flex w-full max-w-[728px] flex-col gap-y-4" method="POST" use:enhance>
+	<div class="flex w-full max-w-[728px] flex-col gap-y-4">
 		{#each data.data as item}
-			<CommentCard
-				comment={{
-					createdAt: item.comments.createdAt ?? '',
-					content: item.comments.content ?? '',
-					score: item.comments.score ?? 0,
-					user: item.users,
-					id: item.comments.id
-				}}
-				{textInput}
-				replies={item.comments.replies ?? []}
-			/>
+			<form method="POST" use:enhance>
+				<CommentCard
+					comment={{
+						createdAt: item.comments.createdAt ?? '',
+						content: item.comments.content ?? '',
+						score: item.comments.score ?? 0,
+						user: item.users,
+						id: item.comments.id
+					}}
+					{textInput}
+					replies={item.comments.replies ?? []}
+				/>
+			</form>
 		{/each}
 
-		{@render textInput({ replying: false, })}
-	</form>
+		<form method="POST" use:enhance>
+			{@render textInput({ replying: false })}
+		</form>
+	</div>
 </main>
 
 <style>
