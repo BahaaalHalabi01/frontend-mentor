eeb8aed0219eb22461672349bcfa1d7b3b3b630b
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-04-27 03:55:42 +0300

    readme

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,11 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: './.env.local' });
 
 export default {
-	schema: './src/lib/server/db/schema.ts',
-	out: './drizzle',
-	driver: 'turso',
-	dbCredentials: {
-		url: process.env.DB_URL!,
-		authToken: process.env.DB_AUTH_TOKEN!
-	}
+  schema: './src/lib/server/db/schema.ts',
+  out: './drizzle',
+  driver: process.env.NODE_ENV === 'dev' ? 'libsql' : 'turso',
+  dbCredentials: {
+    url: process.env.DB_URL!,
+    ...(process.env.NODE_ENV !== 'dev' && { authToken: process.env.DB_AUTH_TOKEN! })
+  }
 } satisfies Config;
